7b4f9638854c9468bee1d5bd578cb2566af7a1ef
Justin Molineaux <justin.molineaux@gmail.com>
2017-04-19 22:08:11 -0700

    Completes specs for absolute time

--- spec/lib/entendido_time_listener_spec.js
+++ spec/lib/entendido_time_listener_spec.js
@@ -22,33 +22,33 @@ describe( 'an instance of EntendidoTimeListener', function() {
 
   describe( 'when used to parse an absolute time', function() {
 
-    describe( 'expressed as "5:15pm"', function() {
+    describe( 'expressed as just an hour', function() {
       beforeEach( function() {
-        result = parseInputWithListener( '5:15pm' );
+        result = parseInputWithListener( '5' );
+      } );
+
+      it( 'determines that the hour is 5', function() {
+        expect( result.hour ).to.equal( 5 );
+      } );
+      it( 'determines that the minute is 0', function() {
+        expect( result.minute ).to.equal( 0 );
+      } );
+    } );
+
+    describe( 'expressed as an hour post meridiem', function() {
+      beforeEach( function() {
+        result = parseInputWithListener( '5p' );
       } );
 
       it( 'determines that the hour is 17', function() {
         expect( result.hour ).to.equal( 17 );
       } );
       it( 'determines that the minute is 0', function() {
-        expect( result.minute ).to.equal( 15 );
+        expect( result.minute ).to.equal( 0 );
       } );
     } );
 
-    describe( 'expressed as "18:30"', function() {
-      beforeEach( function() {
-        result = parseInputWithListener( '18:30' );
-      } );
-
-      it( 'determines that the hour is 18', function() {
-        expect( result.hour ).to.equal( 18 );
-      } );
-      it( 'determines that the minute is 30', function() {
-        expect( result.minute ).to.equal( 30 );
-      } );
-    } )
-
-    describe( 'expressed as 4:32', function() {
+    describe( 'expressed in hours and minutes', function() {
       beforeEach( function() {
         result = parseInputWithListener( '4:32' );
       } );
@@ -61,7 +61,33 @@ describe( 'an instance of EntendidoTimeListener', function() {
       } );
     } );
 
-    describe( 'expressed as 7PM', function() {
+    describe( 'expressed as an hour and minute post meridiem', function() {
+      beforeEach( function() {
+        result = parseInputWithListener( '5:15pm' );
+      } );
+
+      it( 'determines that the hour is 17', function() {
+        expect( result.hour ).to.equal( 17 );
+      } );
+      it( 'determines that the minute is 15', function() {
+        expect( result.minute ).to.equal( 15 );
+      } );
+    } );
+
+    describe( 'expressed in hours/mins, redundantly ante meridiem', function() {
+      beforeEach( function() {
+        result = parseInputWithListener( '4:32am' );
+      } );
+
+      it( 'determines that the hour is 4', function() {
+        expect( result.hour ).to.equal( 4 );
+      } );
+      it( 'determines that the minute is 32', function() {
+        expect( result.minute ).to.equal( 32 );
+      } );
+    } );
+
+    describe( 'expressed as hour, post meridiem, capitalized', function() {
       beforeEach( function() {
         result = parseInputWithListener( '7PM' );
       } );
@@ -71,6 +97,45 @@ describe( 'an instance of EntendidoTimeListener', function() {
       } );
       it( 'determines that the minute is 0', function() {
         expect( result.minute ).to.equal( 0 );
+      } );
+    } );
+
+    describe( 'expressed in 24-hour time with minutes', function() {
+      beforeEach( function() {
+        result = parseInputWithListener( '18:30' );
+      } );
+
+      it( 'determines that the hour is 18', function() {
+        expect( result.hour ).to.equal( 18 );
+      } );
+      it( 'determines that the minute is 30', function() {
+        expect( result.minute ).to.equal( 30 );
+      } );
+    } );
+
+    describe( 'expressed in 24-hour time ante meridiem w/ minutes', function() {
+      beforeEach( function() {
+        result = parseInputWithListener( '18:30am' );
+      } );
+
+      it( 'determines that the hour is 18', function() {
+        expect( result.hour ).to.equal( 18 );
+      } );
+      it( 'determines that the minute is 30', function() {
+        expect( result.minute ).to.equal( 30 );
+      } );
+    } );
+
+    describe( 'expressed in 24-hours, erroneously post meridiem', function() {
+      beforeEach( function() {
+        result = parseInputWithListener( '18:30pm' );
+      } );
+
+      it( 'determines that the hour is 18', function() {
+        expect( result.hour ).to.equal( 18 );
+      } );
+      it( 'determines that the minute is 30', function() {
+        expect( result.minute ).to.equal( 30 );
       } );
     } );
 
